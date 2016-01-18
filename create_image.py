dd8f4c30534e35593c6a0f19e374b4cf9d623d8f
Andrew <agcolello@gmail.com>
2016-01-18 01:34:41 -0500

    working test env

--- create_image.py
+++ create_image.py
@@ -1,15 +1,22 @@
 from duplicate_image import ImageDuplicator
 
 ORIGINAL_PATH = "training_images/rough_orig.png"
+BALL_DIAMETER = 30
 
+dup = ImageDuplicator(ORIGINAL_PATH, BALL_DIAMETER)
+max_width = dup.width - BALL_DIAMETER
+max_length = dup.length - BALL_DIAMETER
 
-dup = ImageDuplicator(ORIGINAL_PATH)
-c = (0,0)
-new_path = dup.draw_circle(c)
-dup.blend_images(ORIGINAL_PATH, new_path, c, 100)
-
-c2 = (0,10)
-new_path2 = dup.draw_circle(c2)
-dup.blend_images(ORIGINAL_PATH, new_path2, c2, 90)
-
+for x in range(0, max_width, BALL_DIAMETER):
+    for y in range(0, max_length, BALL_DIAMETER):
+        print x,y
+        c = list()
+        c.append(x)
+        c.append(y)
+        if x <=30 and y <= 30: #DEV-REMOVE
+            new_path = dup.draw_circle(c)
+            for percent in range(10, 100, 10):
+                if percent == 50 or percent == 80: #DEV-REMOVE
+                    new_path = dup.draw_circle(c)
+                    dup.blend_images(ORIGINAL_PATH, new_path, c, percent)
 
--- duplicate_image.py
+++ duplicate_image.py
@@ -5,54 +5,50 @@ from PIL import Image, ImageFilter, ImageDraw
 
 class ImageDuplicator:
 
-    def __init__(self, orig_path):
+    def __init__(self, orig_path, diameter):
+        self.BALL_DIAMETER = diameter
         # open grass image
         self.grass_path = orig_path
 
         img = Image.open(orig_path) #too big to make a permanent instance variable
         dim = img.size
         self.width = dim[0]
-        self.length = dim[1]
+        self.length = dim[1] #need the width and length when creating loop in higher abstraction level
 
-        del img #special garbage collection 
-
+        del img # garbage collection 
 
     def draw_circle(self, left_corner):
         #create draw object from original path
         img = Image.open(self.grass_path)
         draw_obj = ImageDraw.Draw(img)
 
-        #draw circle with diameter=30 at center input
+        #prepare to draw 
         x0 = left_corner[0]
         y0 = left_corner[1]
-        x1 = x0 + 30 #30x30 box to make circle
-        y1 = y0 + 30
+        x1 = x0 + self.BALL_DIAMETER #create box to draw in
+        y1 = y0 + self.BALL_DIAMETER
         draw_obj.ellipse((x0, y0, x1, y1), fill = 'white', outline ='white')
         
         #alpha is 1.00 for newly drawn circle, represented as 100
         new_path = "training_images/{0}_{1}_100.png".format(x0, y0)
         img.save(new_path)
+
         del draw_obj #garbage collection
         del img 
-        return new_path
+        return new_path #to make it easier to blend
 
-    def blend_images(self, background_path, ball_path, center, percentage):
-        #according to docs at http://effbot.org/imagingbook/image.htm:
-        #out = image1 * (1.0 - alpha) + image2 * alpha
+    def blend_images(self, background_path, ball_path, left_corner, percentage):
+        #according to docs at http://effbot.org/imagingbook/image.htm: out = image1 * (1.0 - alpha) + image2 * alpha
         alpha = percentage/100.0
         background = Image.open(background_path)
         ball = Image.open(ball_path)
 
-        final_path = "training_images/{0}_{1}_{2}.png".format(center[0], center[1], percentage)
+        final_path = "training_images/{0}_{1}_{2}.png".format(left_corner[0], left_corner[1], percentage)
 
         out = Image.blend(background, ball, alpha)
-        out.save(final_path) #example of convention here
-        out.show()
+        out.save(final_path)
+        # out.show()
 
         del background
-        del ball
-
-
-
-
-
+        del ball 
+        del out #collect garbage
